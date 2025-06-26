773546dd15ade8f0e56b061b86e9e46bf23cc7a3
Phil Hetzel <phetzel1@protonmail.com>
2025-06-26 15:45:52 -0400

    backing off custom exporter change

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,14 +1,5 @@
-import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
-import dotenv from 'dotenv';
-dotenv.config();
+import { registerOTel } from "@vercel/otel";
 
 registerOTel({
-  serviceName: 'MastraAppTest',
-  traceExporter: new OTLPHttpJsonTraceExporter({
-    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-    headers: {
-      'Authorization': `Bearer ${process.env.BRAINTRUST_API_KEY}`,
-      'x-bt-parent': 'project_id:fdb997ff-4a7a-419c-942e-ce58070fc5c4',
-    },
-  }),
+  serviceName: 'MastraAppTest'
 });
