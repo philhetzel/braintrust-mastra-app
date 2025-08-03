5e348d7db715d3e7f126b88ce2f1ac325941d6cc
Phil Hetzel <phetzel1@protonmail.com>
2025-08-03 16:32:30 -0400

    cleanup index

--- mastra/index.ts
+++ mastra/index.ts
@@ -1,9 +1,7 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
 
 import { weatherAgent } from './agents/weather-agent';
-import { tavilySearchTool } from './tools/tavily-tool';
 import { weatherActivitiesWorkflow } from './workflows/weather-activities-workflow';
 
 export const mastra = new Mastra({
